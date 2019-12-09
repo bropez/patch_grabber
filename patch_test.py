d6138c4b3c8103c574eb11723f12e6fbfa119eaf
Matthew <matthewbropez@gmail.com>
2019-12-09 15:47:22 -0500

    Ability to get patch list from https://esportstales.com

--- patch_test.py
+++ patch_test.py
@@ -1,58 +1,39 @@
 # patch_test.py
 import os
 
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from site_info import get_img_url
+from site_info import (
+    get_patch_url, get_response, 
+    get_img_url, get_embed
+)
 
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
-patch_url_template = 'https://na.leagueoflegends.com/en/news/game-updates/patch/teamfight-tactics-patch-||||-notes'
-
-
-def get_patch_url(raw_patch_num: str):
-    patch_num = raw_patch_num.replace('.', '')
-    patch_url = patch_url_template.replace('||||', patch_num)
-
-    return patch_url
-
-
-def get_response(patch_num: str, patch_url: str):
-    response = (
-        "Patch number: {}\n"
-        "Check out the patch notes here: {}\n"
-        .format(patch_num, patch_url)
-    )
-
-    return response
-
 
 # bot commands
 @bot.command(name='get_patch', help="Gets the patch notes for the patch that is passed in.")
-async def get_specific_patch(ctx, patch_num: str):
+async def get_patch(ctx, patch_num: str):
     patch_url = get_patch_url(patch_num)
     image_url = get_img_url(patch_url)
 
-    embed = discord.Embed()
-    embed.set_image(url=image_url)
+    embed = get_embed(image_url)
 
     response = get_response(patch_num, patch_url)
     await ctx.send(response, embed=embed)
 
 
 @bot.command(name='recent_patch', help="Gets the most recent hard-coded patch for Team Fight Tactics.")
-async def get_recent_patch(ctx):
+async def recent_patch(ctx):
     patch_num = "9.23"
     patch_url = get_patch_url(patch_num)
     image_url = get_img_url(patch_url)
 
-    embed = discord.Embed()
-    embed.set_image(url=image_url)
+    embed = get_embed(image_url)
     
     response = get_response(patch_num, patch_url)
     await ctx.send(response, embed=embed)
--- site_info.py
+++ site_info.py
@@ -1,9 +1,32 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
+import discord
 
-def get_img_url(patch_url):
-    # url = 'https://na.leagueoflegends.com/en/news/game-updates/patch/teamfight-tactics-patch-923-notes'
+
+patch_url_template = (
+    "https://na.leagueoflegends.com/en/news/game-updates/patch/teamfight-tactics-patch-||||-notes"
+)
+
+
+def get_patch_url(raw_patch_num: str):
+    patch_num = raw_patch_num.replace('.', '')
+    patch_url = patch_url_template.replace('||||', patch_num)
+
+    return patch_url
+
+
+def get_response(patch_num: str, patch_url: str):
+    response = (
+        "Patch number: {}\n"
+        "Check out the patch notes here: {}\n"
+        .format(patch_num, patch_url)
+    )
+
+    return response
+
+
+def get_img_url(patch_url: str):
     page = requests.get(patch_url)
     div_class = "field field-name-field-article-media field-type-file field-label-hidden"
     a_class = "lightbox cboxElement"
@@ -17,5 +40,34 @@ def get_img_url(patch_url):
 
     return img_url
 
+
+def get_embed(img_url: str):
+    embed = discord.Embed()
+    embed.set_image(url=img_url)
+
+    return embed
+
 # TODO: maybe use 'https://www.esportstales.com/teamfight-tactics/patch-notes-list'
 #   to find out which patch is the current patch
+def get_text(el):
+    raw = el.text
+    if ' - ' not in raw:
+        return raw.strip()
+
+    patch = raw.split(' - ')[1]
+    patch_num = patch.split(' ')[1].strip()
+
+    return patch_num
+
+
+
+page = requests.get("https://www.esportstales.com/teamfight-tactics/patch-notes-list")
+table_class = "sqs-block markdown-block sqs-block-markdown"
+
+soup = bs(page.text, 'html.parser')
+table = soup.findAll("div", {"class": table_class})
+text_only = []
+for div in table:
+    text_only.append(get_text(div))
+
+print(text_only)
